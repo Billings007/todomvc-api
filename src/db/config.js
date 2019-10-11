4443e5934f9a7cc7bd9c708ff5f91258968e5a2b
James Billings <parkerbillings@icloud.com>
2019-10-10 18:08:02 -0600

    Update Config.js

--- src/db/config.js
+++ src/db/config.js
@@ -1,25 +1,45 @@
 const options = {
-    query: (e) => {
-      console.log(e.query)
-    }
+  query: (e) => {
+    console.log(e.query)
   }
+}
+
+const pgp = require('pg-promise')(options);
+let db
+try {
+    db = pgp(`postgres://postgres:superMe@localhost:5432/TodoMVC`)
+} catch (e) {
+  console.error('Failed to start application, DB init.', e)
+  process.exit(1)
+}
+
+module.exports = {
+  pgp,
+  db
+}
+
+// const options = {
+//     query: (e) => {
+//       console.log(e.query)
+//     }
+//   }
   
-  const pgp = require('pg-promise')(options);
+//   const pgp = require('pg-promise')(options);
   
-  const setDataBase = () => {
-    if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV){
-      return pgp({
-        user: 'postgres',
-        password: 'superMe',
-        database: 'TodoMVC',
-        port: 5432,
-        host: 'localhost'
-      })
-    }
-    else if (process.env.NODE_ENV === 'production'){
-      return pgp(process.env.DATABASE_URL)
-    }
-  }
+//   const setDataBase = () => {
+//     if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV){
+//       return pgp({
+//         user: 'postgres',
+//         password: 'superMe',
+//         database: 'TodoMVC',
+//         port: 5432,
+//         host: 'localhost'
+//       })
+//     }
+//     else if (process.env.NODE_ENV === 'production'){
+//       return pgp(process.env.DATABASE_URL)
+//     }
+//   }
   
-  const db = setDataBase();
-  module.exports = db;
+//   const db = setDataBase();
+//   module.exports = db;
