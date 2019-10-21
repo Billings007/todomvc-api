111e7c405bce84b6907f9ae018bf24ee43559bb4
James Billings <parkerbillings@icloud.com>
2019-10-21 11:39:45 -0600

    Simplified and added to Index based upon what would support the extension of the app more

--- src/db/config.js
+++ src/db/config.js
@@ -1,13 +1,8 @@
-const options = {
-  query: (e) => {
-    console.log(e.query)
-  }
-}
-
-const pgp = require('pg-promise')(options);
+const pgp = require('pg-promise')();
 let db
+const {DB, DB_HOST, DB_USER, DB_PASSWORD, DB_PORT} = process.env
 try {
-    db = pgp(`postgres://postgres:superMe@localhost:5432/TodoMVC`)
+  db = pgp(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST || 'localhost'}:${DB_PORT || 5432}/${DB}`)
 } catch (e) {
   console.error('Failed to start application, DB init.', e)
   process.exit(1)
@@ -17,29 +12,3 @@ module.exports = {
   pgp,
   db
 }
-
-// const options = {
-//     query: (e) => {
-//       console.log(e.query)
-//     }
-//   }
-  
-//   const pgp = require('pg-promise')(options);
-  
-//   const setDataBase = () => {
-//     if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV){
-//       return pgp({
-//         user: 'postgres',
-//         password: 'superMe',
-//         database: 'TodoMVC',
-//         port: 5432,
-//         host: 'localhost'
-//       })
-//     }
-//     else if (process.env.NODE_ENV === 'production'){
-//       return pgp(process.env.DATABASE_URL)
-//     }
-//   }
-  
-//   const db = setDataBase();
-//   module.exports = db;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
+require('dotenv').load
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 const app = express()
+const cors = require('cors')
+require('./db/config')
 const port = 3000
 
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
-    extended: true,
+    extended: false,
   })
 )
+app.use(express.static(path.join(__dirname, 'public')))
 
-
-app.get('/', (req, res) => {
-    res.json({ info: 'Node.js, Express, and Postgres API' })
-  })
-
-
+app.use(cors())
 app.use('/api/todos', require('./routes/route'))
+
+app.get('*', function(req, res){
+  res.status(404).send('LOL again!?');
+});
+app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
+
+module.exports = app
